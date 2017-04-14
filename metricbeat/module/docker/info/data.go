cdfa5a42352caf31cbee281607f0a4fff304f202
Joe Turgeon <arithmetric@gmail.com>
2017-04-14 16:06:22 -0500

    Extending Docker info module to support Device Mapper storage driver stats.

--- metricbeat/module/docker/info/data.go
+++ metricbeat/module/docker/info/data.go
@@ -3,6 +3,7 @@ package info
 import (
 	"github.com/elastic/beats/libbeat/common"
 
+	units "github.com/docker/go-units"
 	dc "github.com/fsouza/go-dockerclient"
 )
 
@@ -15,7 +16,44 @@ func eventMapping(info *dc.DockerInfo) common.MapStr {
 			"paused":  info.ContainersPaused,
 			"stopped": info.ContainersStopped,
 		},
+		"driver": info.Driver,
 		"images": info.Images,
+	}
+
+	switch driver := info.Driver; driver {
+	case "devicemapper":
+		driverStatus := common.MapStr{}
+		for _, stat := range info.DriverStatus {
+			switch statName := stat[0]; statName {
+			case "Backing Filesystem":
+				driverStatus["backingFilesystem"] = stat[1]
+			case "Base Device Size":
+				driverStatus["baseDeviceSize"], _ = units.FromHumanSize(stat[1])
+			case "Data file":
+				driverStatus["dataFile"] = stat[1]
+			case "Data Space Available":
+				driverStatus["dataSpaceAvailable"], _ = units.FromHumanSize(stat[1])
+			case "Data Space Total":
+				driverStatus["dataSpaceTotal"], _ = units.FromHumanSize(stat[1])
+			case "Data Space Used":
+				driverStatus["dataSpaceUsed"], _ = units.FromHumanSize(stat[1])
+			case "Metadata file":
+				driverStatus["metadataFile"] = stat[1]
+			case "Metadata Space Available":
+				driverStatus["metadataSpaceAvailable"], _ = units.FromHumanSize(stat[1])
+			case "Metadata Space Total":
+				driverStatus["metadataSpaceTotal"], _ = units.FromHumanSize(stat[1])
+			case "Metadata Space Used":
+				driverStatus["metadataSpaceUsed"], _ = units.FromHumanSize(stat[1])
+			case "Pool Blocksize":
+				driverStatus["poolBlocksize"], _ = units.FromHumanSize(stat[1])
+			case "Pool Name":
+				driverStatus["poolName"] = stat[1]
+			case "Thin Pool Minimum Free Space":
+				driverStatus["thinPoolMinimumFreeSpace"], _ = units.FromHumanSize(stat[1])
+			}
+		}
+		event["driverStatus"] = driverStatus
 	}
 
 	return event
